646470ddf9a642684df9dcdd375ff0f2a5903356
ZakiMohammed <shaikhzaki67@gmail.com>
2022-08-02 00:15:22 +0530

    Add project files.

---
+++ AsposeWebApp/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using AsposeWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Docker.Linux.Demo01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _appEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment appEnvironment)
+        {
+            _logger = logger;
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Generate()
+        {
+            const string file_type = "application/pdf";
+            const string file_name = "sample.pdf";
+            var memoryStream = new System.IO.MemoryStream();
+
+            _logger.LogInformation("Start");
+            // Initialize document object
+            var document = new Document();
+            // Add page
+            var page = document.Pages.Add();
+
+            // Add image
+            var imageFileName = System.IO.Path.Combine(_appEnvironment.WebRootPath, "images", "logo.png");
+            page.AddImage(imageFileName, new Rectangle(20, 730, 120, 830));
+
+            // -------------------------------------------------------------
+            // Add Header
+            var header = new TextFragment("New ferry routes in Fall 2020");
+            header.TextState.Font = FontRepository.FindFont("Arial");
+            header.TextState.FontSize = 24;
+            header.HorizontalAlignment = HorizontalAlignment.Center;
+            header.Position = new Position(130, 720);
+            page.Paragraphs.Add(header);
+
+            // Add description
+            var descriptionText = "Visitors must buy tickets online and tickets are limited to 5,000 per day. Ferry service is operating at half capacity and on a reduced schedule. Expect lineups.";
+            var description = new TextFragment(descriptionText);
+            description.TextState.Font = FontRepository.FindFont("Times New Roman");
+            description.TextState.FontSize = 14;
+            description.HorizontalAlignment = HorizontalAlignment.Left;
+            page.Paragraphs.Add(description);
+
+
+            // Add table
+            var table = new Table
+            {
+                ColumnWidths = "200",
+                Border = new BorderInfo(BorderSide.Box, 1f, Color.Black),
+                DefaultCellBorder = new BorderInfo(BorderSide.Box, 0.5f, Color.Gray),
+                DefaultCellPadding = new MarginInfo(4.5, 4.5, 4.5, 4.5),
+                Margin =
+                {
+                    Top = 10,
+                    Bottom = 10
+                },
+                DefaultCellTextState =
+                {
+                    Font =  FontRepository.FindFont("Helvetica")
+                }
+            };
+
+            var headerRow = table.Rows.Add();
+            headerRow.Cells.Add("Departs City");
+            headerRow.Cells.Add("Departs Island");
+            foreach (Cell headerRowCell in headerRow.Cells)
+            {
+                headerRowCell.BackgroundColor = Color.LightGray;
+                headerRowCell.DefaultCellTextState.ForegroundColor = Color.FromRgb(0.1, 0.1, 0.1);
+            }
+
+            var time = new TimeSpan(6, 0, 0);
+            var incTime = new TimeSpan(0, 30, 0);
+            for (int i = 0; i < 10; i++)
+            {
+                var dataRow = table.Rows.Add();
+                dataRow.Cells.Add(time.ToString(@"hh\:mm"));
+                time = time.Add(incTime);
+                dataRow.Cells.Add(time.ToString(@"hh\:mm"));
+            }
+
+            page.Paragraphs.Add(table);
+
+            document.Save(memoryStream);
+            _logger.LogInformation("Finish");
+            return File(memoryStream, file_type, file_name);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
